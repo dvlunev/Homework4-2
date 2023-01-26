cfc0ba9d7bd22d24881272b9649e320942577e8b
dvlunev <dvlunev@ya.ru>
2023-01-26 11:10:57 +0300

    первый коммит

---
+++ src/main/java/me/lunev/homework42/Homework42Application.java
@@ -0,0 +1,13 @@
+package me.lunev.homework42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework42Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework42Application.class, args);
+    }
+
+}
